8de7fc22c68f3744658379f9ed98dc963b626649
IgorMMichels <igor.marcon.michels@gmail.com>
2026-04-28 09:30:38 -0300

    Finished starting project with components and creation of router folder

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,21 @@
 <script setup>
 import { ref } from 'vue'
-import { listaProdutos } from './data/produtos';
+import { listaProdutos } from './data/products';
 import ProductChild from './components/ProductChild.vue';
+import ButtonChild from './components/ButtonChild.vue'
 const produtos = ref(listaProdutos);
+const alterando = ref(false)
+const posicaoProduto = ref(-1)
+const preco = ref(0)
+function corrigirPreco(idProduto) {
+  posicaoProduto.value = produtos.value.findIndex(p => p.id === idProduto);
+  preco.value = produtos.value[posicaoProduto.value].preco
+  alterando.value=true
+}
+function salvarPreco() {
+  produtos.value[posicaoProduto.value].preco = preco.value
+  alterando.value=false
+}
 </script>
 
 <template>
@@ -10,8 +23,20 @@ const produtos = ref(listaProdutos);
     <h1>Catálogo de Produtos</h1>
     <div>
       <ul>
-
+        <ProductChild v-for="produto in produtos"
+        :key="produto.id" :id="produto.id"
+        :nome="produto.nome"
+        :preco="produto.preco"
+        :categoria="produto.categoria"
+        @corrigirpreco="corrigirPreco"
+        >
+        </ProductChild>
       </ul>
+    </div>
+    <div v-show="alterando">
+      <label>Preço</label>
+      <input type="number" v-model.number="preco">
+      <ButtonChild @clique="salvarPreco(preco)">Salvar</ButtonChild>
     </div>
   </div>
 </template>
--- src/components/ProductChild.vue
+++ src/components/ProductChild.vue
@@ -1,13 +1,15 @@
 <script setup>
-
+defineProps(['id', 'nome', 'preco', 'categoria'])
+import ButtonChild from './ButtonChild.vue'
+defineEmits(['corrigirpreco'])
+import { formataPreco } from '@/utils/productUtils'
 </script>
 
 <template>
-  <div>
-
-  </div>
+  <li>
+    {{ nome }} - {{ formataPreco(preco) }} ({{ categoria }})
+    <ButtonChild @clique="$emit('corrigirpreco', id)">Corrigir Preco</ButtonChild>
+  </li>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
