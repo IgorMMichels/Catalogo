2bbb4ef31e8e56c6d3eeca0ef7a416f9c6d6d791
IgorMMichels <igor.marcon.michels@gmail.com>
2026-05-04 11:50:29 -0300

    Router Application and creation off alimentosview and brinquedosview

--- src/App.vue
+++ src/App.vue
@@ -1,44 +1,8 @@
 <script setup>
-import { ref } from 'vue'
-import { listaProdutos } from './data/products';
-import ProductChild from './components/ProductChild.vue';
-import ButtonChild from './components/ButtonChild.vue'
-const produtos = ref(listaProdutos);
-const alterando = ref(false)
-const posicaoProduto = ref(-1)
-const preco = ref(0)
-function corrigirPreco(idProduto) {
-  posicaoProduto.value = produtos.value.findIndex(p => p.id === idProduto);
-  preco.value = produtos.value[posicaoProduto.value].preco
-  alterando.value=true
-}
-function salvarPreco() {
-  produtos.value[posicaoProduto.value].preco = preco.value
-  alterando.value=false
-}
 </script>
 
 <template>
-  <div class="container">
-    <h1>Catálogo de Produtos</h1>
-    <div>
-      <ul>
-        <ProductChild v-for="produto in produtos"
-        :key="produto.id" :id="produto.id"
-        :nome="produto.nome"
-        :preco="produto.preco"
-        :categoria="produto.categoria"
-        @corrigirpreco="corrigirPreco"
-        >
-        </ProductChild>
-      </ul>
-    </div>
-    <div v-show="alterando">
-      <label>Preço</label>
-      <input type="number" v-model.number="preco">
-      <ButtonChild @clique="salvarPreco(preco)">Salvar</ButtonChild>
-    </div>
-  </div>
+  <RouterView/>
 </template>
 
 <style scoped>
---
+++ src/router/index.js
@@ -0,0 +1,20 @@
+import { createRouter, createWebHistory } from 'vue-router';
+
+import HomeView from '@/views/HomeView.vue';
+import AlimentosView from '@/views/AlimentosView.vue';
+import BrinquedosView from '@/views/BrinquedosView.vue';
+import HigieneView from '@/views/HigieneView.vue';
+
+const routes = [
+  { path: '/', component: HomeView },
+  { path: '/alimentos', component: AlimentosView},
+  { path: '/brinquedos', component: BrinquedosView},
+  { path: '/higiene', component: HigieneView},
+]
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+})
+
+export default router
